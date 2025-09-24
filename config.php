e3ec32f86bcc2a4a890f66634c1f65ce36dcf945
yazvip <andriassetiawansubard.o@gmail.com>
2025-09-24 15:28:10 +0700

    Update config.php

--- config.php
+++ config.php
@@ -3,8 +3,8 @@
 // Defaults are safe for local development
 
 $CONFIG = [
-    'client_id' => getenv('BRIAPI_CLIENT_ID') ?: '',
-    'client_secret' => getenv('BRIAPI_CLIENT_SECRET') ?: '',
+    'client_id' => 'bzoZFq23SknHOWovbTMxPgZwAkGIxuCU',
+    'client_secret' => 'xUyEaHFse52GewTP',
     'base_url' => getenv('BRIAPI_BASE_URL') ?: 'https://sandbox.partner.api.bri.co.id',
     'mock_mode' => (getenv('MOCK_MODE') === '1' || strtolower((string) getenv('MOCK_MODE')) === 'true')
 ];
